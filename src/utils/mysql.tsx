39590c00f7eed07540f15809087b518e40d18d85
Dmitri <dimasalt@gmail.com>
2023-09-05 19:02:07 -0400

    fixed no password error in mysql connection utility:

--- src/utils/mysql.tsx
+++ src/utils/mysql.tsx
@@ -3,7 +3,7 @@ import mysql from 'mysql2';
 const pool = mysql.createPool({
         host:process.env.DB_HOST, 
         user: process.env.DB_USER, 
-        password : '',
+        password : process.env.DB_PASS,
         database: process.env.DB_NAME,
         waitForConnections: true,
         connectionLimit: 10,
