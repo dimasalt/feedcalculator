7681a16d3f1414e3413554c2a09c612bd9f42d26
dimasalt <141041615+dimasalt@users.noreply.github.com>
2024-08-30 20:19:44 -0400

    Add database maintenance functionality and updated NPM packages (including next.js)

--- src/app/api/calculator/feeds/get/route.ts
+++ src/app/api/calculator/feeds/get/route.ts
@@ -1,4 +1,5 @@
 
+import { runOnFirstOfMonth } from "@/services/database/dbCleanup";
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -63,43 +64,43 @@ export const POST = async(request: NextRequest)=>  {
 
 
 
-/**
- * ***********************************************************************
- * @description Function runs on first of every month and maintains our feed table by removing old feeds and updating feed_date for current user
- * @param userToken 
- * @returns nothing
- * ***********************************************************************
- */
-const runOnFirstOfMonth = async(userToken: string) => {
+// /**
+//  * ***********************************************************************
+//  * @description Function runs on first of every month and maintains our feed table by removing old feeds and updating feed_date for current user
+//  * @param userToken 
+//  * @returns nothing
+//  * ***********************************************************************
+//  */
+// const runOnFirstOfMonth = async(userToken: string) => {
 
-    //prisma client
-    const prisma = new PrismaClient();
+//     //prisma client
+//     const prisma = new PrismaClient();
 
-    try 
-    {
-        //lets update feed_date for all the feeds in feed table that have same user token as user token
-        //we don't want this functionality to run async as we want to make sure that all existing feeds updated before we delete old feeds        
-        prisma.feed.updateMany({
-            where: {
-                user_token: userToken
-            },
-            data: {
-                feed_date: new Date()
-            }
-        });
+//     try 
+//     {
+//         //lets update feed_date for all the feeds in feed table that have same user token as user token
+//         //we don't want this functionality to run async as we want to make sure that all existing feeds updated before we delete old feeds        
+//         prisma.feed.updateMany({
+//             where: {
+//                 user_token: userToken
+//             },
+//             data: {
+//                 feed_date: new Date()
+//             }
+//         });
 
 
-        //first we need to remove a prisma query all feeds that are older than 1 months and default = 0 and token is same as user token
-        prisma.feed.deleteMany({
-            where: {   
-                NOT: {
-                    user_token: userToken,
-                },                           
-                feed_date: {
-                    lte: new Date(new Date().setMonth(new Date().getMonth() - 1))
-                },
-                is_default: 0
-            }
-        });                
-    } catch (error) {}
-}
+//         //first we need to remove a prisma query all feeds that are older than 1 months and default = 0 and token is same as user token
+//         prisma.feed.deleteMany({
+//             where: {   
+//                 NOT: {
+//                     user_token: userToken,
+//                 },                           
+//                 feed_date: {
+//                     lte: new Date(new Date().setMonth(new Date().getMonth() - 1))
+//                 },
+//                 is_default: 0
+//             }
+//         });                
+//     } catch (error) {}
+// }
---
+++ src/services/database/dbCleanup.ts
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+
+/**
+ * ***********************************************************************
+ * @description Function runs on first of every month and maintains our feed table by removing old feeds and updating feed_date for current user
+ * @param userToken 
+ * @returns nothing
+ * ***********************************************************************
+ */
+
+export const runOnFirstOfMonth = async(userToken: string) => {
+
+    //prisma client
+    const prisma = new PrismaClient();
+
+    try 
+    {
+        //lets update feed_date for all the feeds in feed table that have same user token as user token
+        //we don't want this functionality to run async as we want to make sure that all existing feeds updated before we delete old feeds        
+        prisma.feed.updateMany({
+            where: {
+                user_token: userToken
+            },
+            data: {
+                feed_date: new Date()
+            }
+        });
+
+
+        //first we need to remove a prisma query all feeds that are older than 1 months and default = 0 and token is same as user token
+        prisma.feed.deleteMany({
+            where: {   
+                NOT: {
+                    user_token: userToken,
+                },                           
+                feed_date: {
+                    lte: new Date(new Date().setMonth(new Date().getMonth() - 1))
+                },
+                is_default: 0
+            }
+        });                
+    } catch (error) {}
+}
